c1f5a05804dd6a2da4b4fae26a2ea9d9e93eaee0
pikolos <84343397+pikolos@users.noreply.github.com>
2023-03-30 19:09:48 +0200

    intial commit issue-7

---
+++ compute/vxrack_deploy_wld0_CB.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#goldyck@vmware.com
+
+# $1 : cPod Name
+# This scrips deploys a VCF management domain using an already deployed cloudbuilder.
+
+# source helper functions
+. ./env
+source ./extra/functions.sh
+
+
+
+

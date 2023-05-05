838eaf02c4d5741e1e0c00335fd01faa0a11df75
Eric De Witte <vEDW@users.noreply.github.com>
2023-05-06 01:32:58 +0200

    tnc collection
    Signed-off-by: Eric De Witte <vEDW@users.noreply.github.com>

--- extra/configure_nsxt_atside.sh
+++ extra/configure_nsxt_atside.sh
@@ -807,7 +807,7 @@ check_transport_node_collections() {
         then
                 TNCINFO=$(echo ${RESPONSE} |awk -F '####' '{print $1}')
                 TNCCOUNT=$(echo ${TNCINFO} | jq .result_count)     
-                echo $CCINFO > /tmp/tnc-json 
+                echo $TNCINFO > /tmp/tnc-json 
                 if [[ ${TNCCOUNT} -gt 0 ]]
                 then
                         echo $TNCINFO
@@ -1604,8 +1604,8 @@ then
         MANAGEMENT_NETWORK_ID=$(govc ls -json=true network |jq -r '.elements[].Object.Summary | select (.Name =="VM Network") | .Network.Value')
 fi
 
-OVLYTZID=$(get_transport_zone_id "overlay-tz")
-VLANTZID=$(get_transport_zone_id "edge-vlan-tz")
+OVLYTZID=$(get_transport_zone_path "overlay-tz")
+VLANTZID=$(get_transport_zone_path "edge-vlan-tz")
 
 #UPLINKPROFILEID=$(get_uplink_profile_id "edge-profile")
 UPLINKPROFILEID=$(get_uplink_profile_path "edge-profile")

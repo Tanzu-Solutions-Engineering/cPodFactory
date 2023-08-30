2e538511a13ad5c6bfc749fbcb13f76bf3bf6ba0
Eric De Witte <vEDW@users.noreply.github.com>
2023-08-30 09:53:08 +0200

    vcsa
    Signed-off-by: Eric De Witte <vEDW@users.noreply.github.com>

--- extra/deploy_vcsa_only.sh
+++ extra/deploy_vcsa_only.sh
@@ -123,16 +123,18 @@ bash ${MYSCRIPT}
 
 ONCE=0
 STATUS="RUNNING"
+printf "\t Installing VCSA "
 while [ "${STATUS}" != "SUCCEEDED" ]
 do
 	sleep 5
-	echo "Installing..."
+    printf '.' >/dev/tty
 	STATUS=$( curl -s -k -u administrator@${AUTH_DOMAIN}:${PASSWORD} -X GET https://${IP}:5480/rest/vcenter/deployment )
 	echo ${STATUS} | grep ".status" 2>&1 > /dev/null && STATUS=$( echo ${STATUS} | jq '.status' | sed 's/"//g' )	
 	
 	if [ "${STATUS}" == "RUNNING" ] && [ ${ONCE} -eq 0 ]; then
 		ONCE=1
 		echo "Follow the deployment trough https://vcsa.${DOMAIN}:5480"
+		printf "\t Installing VCSA "
 	fi
 done	
 

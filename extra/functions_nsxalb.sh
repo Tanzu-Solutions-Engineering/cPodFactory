2915674c51652be951a3a8e6fe6766730c582c51
Eric De Witte <vEDW@users.noreply.github.com>
2023-10-29 14:39:12 +0100

    test configure
    Signed-off-by: Eric De Witte <vEDW@users.noreply.github.com>

--- extra/functions_nsxalb.sh
+++ extra/functions_nsxalb.sh
@@ -58,7 +58,7 @@ login_nsxalb() {
 
 get_cluster_info(){
         # get Cluster info json
-        RESPONSE=$(curl -s -k -w '####%{response_code}' "${curlArgs[@]}" -d '{"username":"admin", "password":"'${PASSWORD}'"}' -X GET https://${NSXALBFQDN}/api/cluster   -b /tmp/cookies.txt)
+        RESPONSE=$(curl -s -k -w '####%{response_code}' "${curlArgs[@]}" -X GET https://${NSXALBFQDN}/api/cluster   -b /tmp/cookies.txt)
         HTTPSTATUS=$(echo ${RESPONSE} |awk -F '####' '{print $2}')
 
         if [ $HTTPSTATUS -eq 200 ]
@@ -75,7 +75,7 @@ get_cluster_info(){
 
 get_cluster_uuid(){
         # get clusterUUID
-        RESPONSE=$(curl -s -k -w '####%{response_code}' "${curlArgs[@]}" -d '{"username":"admin", "password":"'${PASSWORD}'"}' -X GET https://${NSXALBFQDN}/api/cloud   -b /tmp/cookies.txt)
+        RESPONSE=$(curl -s -k -w '####%{response_code}' "${curlArgs[@]}" -X GET https://${NSXALBFQDN}/api/cloud   -b /tmp/cookies.txt)
         HTTPSTATUS=$(echo ${RESPONSE} |awk -F '####' '{print $2}')
 
         if [ $HTTPSTATUS -eq 200 ]
@@ -110,9 +110,45 @@ vcenter_verify_login(){
         then
                 RESPONSEJSON=$(echo ${RESPONSE} |awk -F '####' '{print $1}')
                 #echo "Response : "
-                echo ${RESPONSEJSON} |jq .
+                echo ${RESPONSEJSON} |jq .resource.vcenter_dc_info[].name
         else
                 echo "error verifying vcenter login"
+                echo ${HTTPSTATUS}
+                echo ${RESPONSE}
+                exit
+        fi
+
+}
+
+configure_defaultcloud_vcenter(){
+
+# curl 'https://nsxalb01.cpod-v8alb.az-lhr.cloud-garage.net/api/cloud/cloud-89a795f5-52e1-4d23-8184-6e9c992d0aea?include_name' -X PUT -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/118.0' -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'Referer: https://nsxalb01.cpod-v8alb.az-lhr.cloud-garage.net/' -H 'X-Avi-UserAgent: UI' -H 'X-Avi-Version: 22.1.4' -H 'X-Avi-Tenant: admin' -H 'Content-Type: application/json;charset=utf-8' -H 'X-CSRFToken: Ix4pDXABLlZcjkNr3NmkHEKWwAIQoRJm' -H 'Origin: https://nsxalb01.cpod-v8alb.az-lhr.cloud-garage.net' -H 'Connection: keep-alive' -H 'Cookie: csrftoken=Ix4pDXABLlZcjkNr3NmkHEKWwAIQoRJm; avi-sessionid=moanoz5jflrgs1cfpzi73puhy03wc8az; sessionid=moanoz5jflrgs1cfpzi73puhy03wc8az' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin' -H 'TE: trailers' 
+#       --data-raw '{"url":"https://nsxalb01.cpod-v8alb.az-lhr.cloud-garage.net/api/cloud/cloud-89a795f5-52e1-4d23-8184-6e9c992d0aea#Default-Cloud","uuid":"cloud-89a795f5-52e1-4d23-8184-6e9c992d0aea","name":"Default-Cloud","tenant_ref":"https://nsxalb01.cpod-v8alb.az-lhr.cloud-garage.net/api/tenant/admin#admin","_last_modified":"1698323383028197","autoscale_polling_interval":60,"dhcp_enabled":false,"dns_resolution_on_se":false,"enable_vip_on_all_interfaces":false,"enable_vip_static_routes":false,"ip6_autocfg_enabled":false,"maintenance_mode":false,"metrics_polling_interval":300,"mtu":1500,"prefer_static_routes":false,"state_based_dns_registration":true,"vmc_deployment":false,"vtype":"CLOUD_VCENTER","vcenter_configuration":{"privilege":"WRITE_ACCESS","use_content_lib":false,"is_nsx_environment":false,"datacenter":"cPod-V8ALB","vcenter_url":"vcsa.cpod-v8alb.az-lhr.cloud-garage.net","username":"administrator@cpod-v8alb.az-lhr.cloud-garage.net","password":"NlPlnFh1vbF!"}}'
+        CLOUDUUID=${1}
+        USERNAME=${2}
+        PASSWORD=${3}
+        VCENTER_FQDN=${4}
+        DATACENTER=${5}
+
+        DATA='{"url":"https://'"${NSXALBFQDN}"'/api/cloud/'"${CLOUDUUID}"'#Default-Cloud","uuid":"'"${CLOUDUUID}"'","name":"Default-Cloud",
+        "tenant_ref":"https://'"${NSXALBFQDN}"'/api/tenant/admin#admin","autoscale_polling_interval":60,"dhcp_enabled":false,"dns_resolution_on_se":false,
+        "enable_vip_on_all_interfaces":false,"enable_vip_static_routes":false,"ip6_autocfg_enabled":false,"maintenance_mode":false,
+        "metrics_polling_interval":300,"mtu":1500,"prefer_static_routes":false,"state_based_dns_registration":true,"vmc_deployment":false,
+        "vtype":"CLOUD_VCENTER","vcenter_configuration":{"privilege":"WRITE_ACCESS","use_content_lib":false,"is_nsx_environment":false,
+        "datacenter":"'"${DATACENTER}"'","vcenter_url":"'"${VCENTER_FQDN}"'",
+        "username":"'"${USERNAME}"'","password":"'"${PASSWORD}"'"}}'
+        SCRIPT="/tmp/DATA-$$"
+        echo ${DATA} > ${SCRIPT}
+        RESPONSE=$(curl -s -k -w '####%{response_code}' "${curlArgs[@]}" -X PUT -d @${SCRIPT} https://${NSXALBFQDN}/api/cloud/"${CLOUDUUID}"?include_name -b /tmp/cookies.txt)
+        HTTPSTATUS=$(echo ${RESPONSE} |awk -F '####' '{print $2}')
+
+        if [ $HTTPSTATUS -eq 200 ]
+        then
+                RESPONSEJSON=$(echo ${RESPONSE} |awk -F '####' '{print $1}')
+                #echo "Response : "
+                echo ${RESPONSEJSON} |jq .
+        else
+                echo "error configuring default cloud vcenter"
                 echo ${HTTPSTATUS}
                 echo ${RESPONSE}
                 exit

469e3f759e6a4ab9afe5ee0380c1679f638fafe0
Eric De Witte <vEDW@users.noreply.github.com>
2023-11-11 15:43:28 +0100

    settings
    Signed-off-by: Eric De Witte <vEDW@users.noreply.github.com>

--- compute/cloudbuilder_lab_settings.sh
+++ compute/cloudbuilder_lab_settings.sh
@@ -4,12 +4,15 @@
 
 [ -f /home/admin/labsettings_done ] && echo "Lab settings already set" && exit 
 
-echo "#Lab sizing entries" >>/etc/vmware/vcf/domainmanager/application-prod.properties
-echo "nsxt.manager.formfactor=small" >>/etc/vmware/vcf/domainmanager/application-prod.properties
-echo "nsxt.manager.wait.minutes=45" >>/etc/vmware/vcf/domainmanager/application-prod.properties
-echo "nsxt.manager.cluster.size=1" >>/etc/vmware/vcf/domainmanager/application-prod.properties
-echo "nsxt.management.resources.validation.skip=true" >>/etc/vmware/vcf/domainmanager/application-prod.properties
-echo "vc7.deployment.option:small" >>/etc/vmware/vcf/domainmanager/application-prod.properties
+SETTINGSFILE=$(find / -name application-prod*)
+echo "${SETTINGSFILE}"
+
+echo "#Lab sizing entries" >> ${SETTINGSFILE}
+echo "nsxt.manager.formfactor=small" >> ${SETTINGSFILE}
+echo "nsxt.manager.wait.minutes=45" >> ${SETTINGSFILE}
+echo "nsxt.manager.cluster.size=1" >> ${SETTINGSFILE}
+echo "nsxt.management.resources.validation.skip=true" >> ${SETTINGSFILE}
+echo "vc7.deployment.option:small" >> ${SETTINGSFILE}
 
 systemctl restart domainmanager
 touch /home/admin/labsettings_done

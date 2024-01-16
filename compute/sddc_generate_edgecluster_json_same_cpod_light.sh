4a3ecd6ad57c282bbd28c3fac8eb367dae30f576
Eric De Witte <vEDW@users.noreply.github.com>
2024-01-16 14:36:19 +0100

    test
    Signed-off-by: Eric De Witte <vEDW@users.noreply.github.com>

--- compute/sddc_generate_edgecluster_json_same_cpod_light.sh
+++ compute/sddc_generate_edgecluster_json_same_cpod_light.sh
@@ -64,7 +64,7 @@ fi
 T0ULVLAN01=5
 T0ULVLAN02=6
 
-if [ ${VLAN} -gt 40 ]; then
+if [[ ${VLAN} -gt 40 ]]; then
         EDGETEPVLANID="${VLAN}4"
         T0ULVLANID01="${VLAN}${T0ULVLAN01}"
         T0ULVLANID02="${VLAN}${T0ULVLAN02}"
@@ -176,32 +176,32 @@ T0NAME="${WLDNAME}-t0"
 T1NAME="${WLDNAME}-t1"
 
 # Generate JSON for cloudbuilder
-sed -i -e "s/###EDGECLUSTERNAME###/${EDGECLUSTERNAME}/g" ${SCRIPT}
-sed -i -e "s/###PASSWORD###/${PASSWORD}/" ${SCRIPT}
-sed -i -e "s/${SCRIPT}"###ASNNUMBER###${SCRIPT}"/${ASNNSXT}/" ${SCRIPT}
-sed -i -e "s/###EDGE01FQDN###/${EDGE01FQDN}/" ${SCRIPT}
-sed -i -e "s/###EDGE02FQDN###/${EDGE02FQDN}/" ${SCRIPT}
-sed -i -e "s/###EDGE01MGMTIP###/${EN01IP}/" ${SCRIPT}
-sed -i -e "s/###EDGE02MGMTIP###/${EN02IP}/" ${SCRIPT}
-sed -i -e "s/###MGMTGW###/${ENMGMTGW}/" ${SCRIPT}
-sed -i -e "s/###MGMTPORTGROUPNAME###/${MGMTPORTGROUPNAME}/" ${SCRIPT}
-sed -i -e "s/###EDGE01TEPIP01###/${EDGE01TEPIP01}/" ${SCRIPT}
-sed -i -e "s/###EDGE01TEPIP02###/${EDGE01TEPIP02}/" ${SCRIPT}
-sed -i -e "s/###EDGE02TEPIP01###/${EDGE02TEPIP01}/" ${SCRIPT}
-sed -i -e "s/###EDGE02TEPIP02###/${EDGE02TEPIP02}/" ${SCRIPT}
-sed -i -e "s/###EDGETEPGW###/${EDGETEPGW}/" ${SCRIPT}
-sed -i -e "s/${SCRIPT}"###EDGETEPVLANID###${SCRIPT}"/${EDGETEPVLANID}/" ${SCRIPT}
-sed -i -e "s/${SCRIPT}"###UPLINK01VLANID###${SCRIPT}"/${T0ULVLANID01}/" ${SCRIPT}
-sed -i -e "s/${SCRIPT}"###UPLINK02VLANID###${SCRIPT}"/${T0ULVLANID02}/" ${SCRIPT}
-sed -i -e "s/###T0UPLINKIP01###/${T0IP01}/" ${SCRIPT}
-sed -i -e "s/###T0UPLINKIP02###/${T0IP03}/" ${SCRIPT}
-sed -i -e "s/###T0UPLINKIP03###/${T0IP02}/" ${SCRIPT}
-sed -i -e "s/###T0UPLINKIP04###/${T0IP04}/" ${SCRIPT}
-sed -i -e "s/###T0ULGW01###/${T0ULGW01}/" ${SCRIPT}
-sed -i -e "s/###T0ULGW02###/${T0ULGW02}/" ${SCRIPT}
-sed -i -e "s/${SCRIPT}"###CPODROUTERASN###${SCRIPT}"/${ASNCPOD}/" ${SCRIPT}
-sed -i -e "s/###T0NAME###/${T0NAME}/" ${SCRIPT}
-sed -i -e "s/###T1NAME###/${T1NAME}/" ${SCRIPT}
+echo sed -i -e "s/###EDGECLUSTERNAME###/${EDGECLUSTERNAME}/g" ${SCRIPT}
+echo sed -i -e "s/###PASSWORD###/${PASSWORD}/" ${SCRIPT}
+echo sed -i -e "s/${SCRIPT}"###ASNNUMBER###${SCRIPT}"/${ASNNSXT}/" ${SCRIPT}
+echo sed -i -e "s/###EDGE01FQDN###/${EDGE01FQDN}/" ${SCRIPT}
+echo sed -i -e "s/###EDGE02FQDN###/${EDGE02FQDN}/" ${SCRIPT}
+echo sed -i -e "s/###EDGE01MGMTIP###/${EN01IP}/" ${SCRIPT}
+echo sed -i -e "s/###EDGE02MGMTIP###/${EN02IP}/" ${SCRIPT}
+echo sed -i -e "s/###MGMTGW###/${ENMGMTGW}/" ${SCRIPT}
+echo sed -i -e "s/###MGMTPORTGROUPNAME###/${MGMTPORTGROUPNAME}/" ${SCRIPT}
+echo sed -i -e "s/###EDGE01TEPIP01###/${EDGE01TEPIP01}/" ${SCRIPT}
+echo sed -i -e "s/###EDGE01TEPIP02###/${EDGE01TEPIP02}/" ${SCRIPT}
+echo sed -i -e "s/###EDGE02TEPIP01###/${EDGE02TEPIP01}/" ${SCRIPT}
+echo sed -i -e "s/###EDGE02TEPIP02###/${EDGE02TEPIP02}/" ${SCRIPT}
+echo sed -i -e "s/###EDGETEPGW###/${EDGETEPGW}/" ${SCRIPT}
+echo sed -i -e "s/${SCRIPT}"###EDGETEPVLANID###${SCRIPT}"/${EDGETEPVLANID}/" ${SCRIPT}
+echo sed -i -e "s/${SCRIPT}"###UPLINK01VLANID###${SCRIPT}"/${T0ULVLANID01}/" ${SCRIPT}
+echo sed -i -e "s/${SCRIPT}"###UPLINK02VLANID###${SCRIPT}"/${T0ULVLANID02}/" ${SCRIPT}
+echo sed -i -e "s/###T0UPLINKIP01###/${T0IP01}/" ${SCRIPT}
+echo sed -i -e "s/###T0UPLINKIP02###/${T0IP03}/" ${SCRIPT}
+echo sed -i -e "s/###T0UPLINKIP03###/${T0IP02}/" ${SCRIPT}
+echo sed -i -e "s/###T0UPLINKIP04###/${T0IP04}/" ${SCRIPT}
+echo sed -i -e "s/###T0ULGW01###/${T0ULGW01}/" ${SCRIPT}
+echo sed -i -e "s/###T0ULGW02###/${T0ULGW02}/" ${SCRIPT}
+echo sed -i -e "s/${SCRIPT}"###CPODROUTERASN###${SCRIPT}"/${ASNCPOD}/" ${SCRIPT}
+echo sed -i -e "s/###T0NAME###/${T0NAME}/" ${SCRIPT}
+echo sed -i -e "s/###T1NAME###/${T1NAME}/" ${SCRIPT}
 
 
-echo "JSON is genereated: ${SCRIPT} and placed in directory: ${SCRIPT_DIR}."
+echo "JSON is generated: ${SCRIPT} and placed in directory: ${SCRIPT_DIR}."

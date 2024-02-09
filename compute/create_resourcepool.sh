858c5cfb88d89dc6c31bd1de33d7b4e4c278fa63
Eric De Witte <vEDW@users.noreply.github.com>
2024-02-09 14:04:39 +0100

    cpodrouter mtu
    Signed-off-by: Eric De Witte <vEDW@users.noreply.github.com>

--- compute/create_resourcepool.sh
+++ compute/create_resourcepool.sh
@@ -43,4 +43,22 @@ ${SCRIPT}
 echo "Creating ResourcePool '${HEADER}-${1}' with ${4} Nested ESXi."
 docker run --rm --dns=${DNS} --entrypoint="/usr/bin/pwsh" -v /tmp/scripts:/tmp/scripts vmware/powerclicore:12.4 ${SCRIPT} 2>&1 > /dev/null
 
+#set mtu 9000 on all cpodrouter interfaces
+CPODROUTER=$( echo "${HEADER}-${1}" | tr '[:upper:]' '[:lower:]' )
+
+#wait for ESXCLI to become available 
+while [ "$SSHOK" != 0 ]
+do  
+SSHOK=$( ssh -o "StrictHostKeyChecking=no" -o "ConnectTimeout=5" -o "UserKnownHostsFile=/dev/null" -o "LogLevel=error" root@"${CPODROUTER}" exit >/dev/null 2>&1; echo $? ) 
+echo "SSH status $CPODROUTER ===$SSHOK==="
+sleep 10
+TIMEOUT=$((TIMEOUT + 1))
+if [ $TIMEOUT -ge 20 ]; then
+    echo "bailing out..."
+    exit 1  
+fi 
+done
+
+ssh -o LogLevel=error -o StrictHostKeyChecking=no root@"$CPODROUTER" "ip link | grep eth | grep mtu | awk '{print $2}' | cut -d '@' -f1 | cut -d ':' -f1 |xargs -n1 ip link set mtu 9000 dev"
+  
 #rm -fr ${SCRIPT}
